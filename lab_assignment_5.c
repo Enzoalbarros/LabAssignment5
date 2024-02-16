6051fe8601d9cab7585b818c82f39dbb7e712c8d
EnzoAlisson <enzoalbarros@gmail.com>
2024-02-15 21:48:43 -0500

    git commit git commit
    git commit files

---
+++ lab_assignment_5.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    char letter;
+    struct node* next;
+} node;
+
+// Returns number of nodes in the linked list.
+int length(node* head) {
+    int count = 0;
+    while (head != NULL) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+// Parses the string in the linked list.
+// If the linked list is head -> |a|->|b|->|c|,
+// then toCString function will return "abc".
+char* toCString(node* head) {
+    int len = length(head);
+    char* str = (char*)malloc((len + 1) * sizeof(char));
+    int i = 0;
+    while (head != NULL) {
+        str[i++] = head->letter;
+        head = head->next;
+    }
+    str[i] = '\0';
+    return str;
+}
+
+// Inserts character to the linked list.
+// If the linked list is head -> |a|->|b|->|c|,
+// then insertChar(&head, 'x') will update the linked list as follows:
+// head -> |a|->|b|->|c|->|x|.
+void insertChar(node** pHead, char c) {
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->letter = c;
+    newNode->next = NULL;
+
+    if (*pHead == NULL) {
+        *pHead = newNode;
+    } else {
+        node* temp = *pHead;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+// Deletes all nodes in the linked list.
+void deleteList(node** pHead) {
+    node* current = *pHead;
+    node* next;
+
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
+    }
+
+    *pHead = NULL;
+}
+
+int main(void)
+{
+	int i, strLen, numInputs;
+	node* head = NULL;
+	char* str;
+	char c;
+	FILE* inFile = fopen("input.txt","r");
+
+	fscanf(inFile, " %d\n", &numInputs);
+	
+	while (numInputs -- > 0)
+	{
+		fscanf(inFile, " %d\n", &strLen);
+		for (i = 0; i < strLen; i++)
+		{
+			fscanf(inFile," %c", &c);
+			insertChar(&head, c);
+		}
+
+		str = toCString(head);
+		printf("String is : %s\n", str);
+
+		free(str);
+		deleteList(&head);
+
+		if (head != NULL)
+		{
+			printf("deleteList is not correct!");
+			break;
+		}
+	}
+
+	fclose(inFile);
+}
